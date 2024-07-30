adeedfa217075788d0bab38b505e8f4cb7d57333
Darren Shepherd <darren@acorn.io>
2024-07-30 13:44:26 -0700

    chore: tweak workspace to format similar to cwd tool

--- workspace/workspace.py
+++ workspace/workspace.py
@@ -6,21 +6,21 @@ dir = os.environ.get('GPTSCRIPT_WORKSPACE_DIR', '')
 if not os.path.isdir(dir):
     sys.exit(0)
 
-print(f'The workspace directory is "{dir}". Use "{dir}" to create new files or directories.')
-
 files = os.listdir(dir)
-if not files:
-    sys.exit(0)
-
 files.sort()
 if len(files) > 0:
-    print(f'The workspace contains the following files and directories:')
+    print(f'The workspace directory is "{dir}" and contains the following files and directories:')
     for file in files:
         if file.startswith('.'):
             continue
         if os.path.isdir(os.path.join(dir, file)):
-            print(f'  {file}/')
+            print(f'- {file}/')
         else:
-            print(f'  {file}')
-print("Always use absolute paths to interact with files in the workspace. The workspace path is not the same as the current working directory.")
+            print(f'- {file}')
+else:
+    print(f'The workspace directory is "{dir}" and contains no files.')
+
+print("Always use absolute paths to interact with files in the workspace.")
+print("Prefer creating new files in the workspace if the user does not imply a specific directory.")
+print("If the user refers to '.' that is the current working directory and not the workspace directory.")
 print()
